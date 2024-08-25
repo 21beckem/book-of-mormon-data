9dbd6e68b45f634f760cf8ef25c388894ce64114
Michael Becker <51341849+21beckem@users.noreply.github.com>
2024-08-25 13:52:39 -0500

    updated to a more condenced format

---
+++ book_list.js
@@ -0,0 +1,17 @@
+const book_list = [
+	"1 Nephi",
+	"2 Nephi",
+	"Jacob",
+	"Enos",
+	"Jarom",
+	"Omni",
+	"Words of Mormon",
+	"Mosiah",
+	"Alma",
+	"Helaman",
+	"3 Nephi",
+	"4 Nephi",
+	"Mormon",
+	"Ether",
+	"Moroni"
+]
